a335518b14f3d51389e800b7645c50df9cd09129
xenonrazels <xenonrazels@gmail.com>
2022-04-05 05:30:52 +0545

    first commit

---
+++ random.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+#inlcude<vector>
+using namespace std;
+
+int main(){
+    int numberOfRand=5
+    vector<int> randomList(numberOfRand);
+    for (int x=1;x<=numberOfRand;x++){
+        randomList[1]
+    }
+
+}
